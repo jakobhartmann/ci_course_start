4e84f272e0e926f26076af2b6e9122e8fcf3c7d3
Jakob Hartmann <hartmann.jakob@web.de>
2025-10-20 13:50:42 +0100

    docs: add Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,28 @@ author = 'Jakob Hartmann'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+import os
+import sys
+
+# Make sure the project root is importable so autodoc can import the package.
+# docs/ is inside the project root, so add the parent directory.
+sys.path.insert(0, os.path.abspath('..'))
+
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.napoleon',
+	'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Autodoc default options: include members by default
+autodoc_default_options = {
+	'members': True,
+	'undoc-members': True,
+	'show-inheritance': True,
+}
 
 
 
---
+++ docs/functionality.rst
@@ -0,0 +1,7 @@
+ci_course.functionality
+=======================
+
+.. automodule:: ci_course.functionality
+    :members:
+    :undoc-members:
+    :show-inheritance:
